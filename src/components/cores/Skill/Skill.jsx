056fdc5bfdd7ab541b89d8aaf3a73fc10352de4d
ZiedMabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2022-10-04 04:25:12 +0100

    waw

--- src/components/cores/Skill/Skill.jsx
+++ src/components/cores/Skill/Skill.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Skill.scss";
+import Aos from "aos";
 
-const Skill = () => {
+const Skill = ({ level, title }) => {
+  React.useEffect(() => {
+    Aos.init({ duration: 1000, delay: 200 });
+  }, []);
   return (
-    <div className='skill'>
-        
+    <div className="skill" data-aos="fade-up" data-aos-once="true">
+      <img src={"./img/skills/" + title + ".svg"} alt={"g"} />
+      <h2>{title}</h2>
+      <p>{level}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Skill
+export default Skill;
--- src/components/modules/SideBar/SideBar.jsx
+++ src/components/modules/SideBar/SideBar.jsx
@@ -27,19 +27,23 @@ const SideBar = () => {
           <span>22</span>
         </div>
       </div>
-      <div className="seperator"></div>
       <div className="sidebar-languages">
         <ProgressBar percentage={100} title={"french"} />
         <ProgressBar percentage={90} title={"arabic"} />
         <ProgressBar percentage={80} title={"english"} />
       </div>
-      <div className="seperator"></div>
       <div className="sidebar-skills">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        <Skill level={'Advanced'} title={"Java"}  />
+        <Skill level={"Advanced"} title={"HTML"}  />
+        <Skill level={"Advanced"} title={"CSS"}  />
+        <Skill level={"Advanced"} title={"Javascript"} />
+        <Skill level={"Advanced"} title={"PHP"}  />
+        <Skill level={"Advanced"} title={"SQL"}  />
+        <Skill level={"Intermediate"} title={"C++"}  />
+        <Skill level={"Intermediate"} title={"Symfony"}  />
+        <Skill level={"Intermediate"} title={"Python"}  />
+        <Skill level={"Advanced"} title={"React"}  />
+        <Skill level={"Basic"} title={"Angular"}  />
       </div>
     </div>
   );
