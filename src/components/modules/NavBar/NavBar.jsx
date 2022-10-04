822b1fcf07b39610927d0fc98da5686bec8123d8
ZiedMabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2022-10-04 21:03:44 +0100

    waw

--- src/components/modules/NavBar/NavBar.jsx
+++ src/components/modules/NavBar/NavBar.jsx
@@ -4,13 +4,34 @@ import "./NavBar.scss";
 const NavBar = ({ active, setActive }) => {
   return (
     <div className={"navbar" + (active ? " active-navbar" : "")}>
-      <button onClick={() => setActive(!active)}>click</button>
       <div className="navbar-header">
-        <svg viewBox="0 0 100 80" width="20" height="20">
-          <rect width="100" height="20"></rect>
-          <rect y="30" width="100" height="20"></rect>
-          <rect y="60" width="100" height="20"></rect>
-        </svg>
+        {active ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 60 60"
+            width="20"
+            height="20"
+            overflow="visible"
+            stroke="white"
+            strokeWidth="6"
+            strokeLinecap="round"
+            onClick={() => setActive(!active)}
+          >
+            <line x1="0" y1="0" x2="50" y2="50" />
+            <line x1="50" y1="0" x2="0" y2="50" />
+          </svg>
+        ) : (
+          <svg
+            viewBox="0 0 100 80"
+            width="20"
+            height="20"
+            onClick={() => setActive(!active)}
+          >
+            <rect width="100" height="12"></rect>
+            <rect y="30" width="100" height="12"></rect>
+            <rect y="60" width="100" height="12"></rect>
+          </svg>
+        )}
       </div>
     </div>
   );
