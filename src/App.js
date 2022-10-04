3fa477c356bdd33f6f4f55d6d9f14795913c2892
ZiedMabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2022-10-04 12:13:07 +0100

    waw

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
+import React from "react";
 import "./App.scss"
+import Home from "./components/modules/Home/Home";
 import SideBar from "./components/modules/SideBar/SideBar";
 
 function App() {
+  let [selected,setSelected] = React.useState(<Home/>)
   return (
     <div className="app">
       <div className="container">
-
         <SideBar />
+        {selected}
       </div>
     </div>
   );
