4380c0ed606b4eefff6340d3cb05d59de13f5f60
ZiedMabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2022-10-15 19:42:36 +0100

    waw

--- src/components/cores/EducationCard/EducationCard.jsx
+++ src/components/cores/EducationCard/EducationCard.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import "./EducationCard.scss"
+import React from "react";
+import "./EducationCard.scss";
 
-const EducationCard = ({card}) => {
-  const startDate = card.date.split("-")[0]
-  const endDate = card.date.split("-")[1]
+const EducationCard = ({ card }) => {
+  const startDate = card.date.split("-")[0];
+  const endDate = card.date.split("-")[1];
   return (
     <div className="education-card">
-        <div>
-
+       <div className="box-inner">
+      <div>
         <h1>{card.title}</h1>
-        <span>{startDate +"-"} <b>{endDate}</b></span>
-        </div>
-        <h2>{card.role}</h2>
-        <p>
-          {card.description}
-        </p>
+        <span>
+          {card.date }
+        </span>
+      
+     </div>
+      <h2>{card.role}</h2>
+      <p>{card.description}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EducationCard
+export default EducationCard;
