35d4d013971a7384c904b72fc4e0558b90d4cb74
ZiedMabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2022-10-15 08:50:05 +0100

    waw

--- src/components/cores/ProgressBar/ProgressBar.jsx
+++ src/components/cores/ProgressBar/ProgressBar.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import "./ProgressBar.scss";
 
 const ProgressBar = ({ title, percentage }) => {
+  var [count, setCount] = React.useState(0);
+  React.useEffect(() => {
+      const inter = setInterval(()=>{
+        setCount(count + 1);
+        console.log("added");
+      }, 1000);
+      if(percentage <= count )
+        clearInterval(inter)
+  }, [percentage]);
   return (
     <div className="progressbar">
-        <div className="progressbar-content">
-        <span>{percentage + "%"}</span>
+      <div className="progressbar-content">
+        <span>{count + "%"}</span>
       </div>
-      <svg viewBox="0 0 100 100" >
+      <svg viewBox="0 0 100 100">
         <path
           d="M 50,50 m 0,-46.5 a 46.5,46.5 0 1 1 0,93 a 46.5,46.5 0 1 1 0,-93"
           stroke="#eee"
@@ -19,12 +28,10 @@ const ProgressBar = ({ title, percentage }) => {
           stroke="#555"
           strokeWidth="7"
           fillOpacity="0"
-          className={"path-"+percentage}
+          className={"path-" + percentage}
         ></path>
       </svg>
-      <h2 className="progressbar-title">
-        {title}
-      </h2>
+      <h2 className="progressbar-title">{title}</h2>
     </div>
   );
 };
